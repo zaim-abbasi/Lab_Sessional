0607468f59ad4651c7b60336a26772bf99a52d9c
Zaim Abbasi <zaim.ka@outlook.com>
2025-04-18 12:18:22 +0500

    fixed http issue

--- Cafe_Management_System/api-gateway/index.js
+++ Cafe_Management_System/api-gateway/index.js
@@ -10,9 +10,8 @@ app.use('/menu', createProxyMiddleware({ target: 'http://menu-service:3001', cha
 app.use('/orders', createProxyMiddleware({ target: 'http://order-service:3002', changeOrigin: true }));
 app.use('/payments', createProxyMiddleware({ target: 'http://payment-service:3003', changeOrigin: true }));
 app.use('/inventory', createProxyMiddleware({ target: 'http://inventory-service:3004', changeOrigin: true }));
-app.use('/customers', createProxyMiddleware({ target: 'customer-service:3005', changeOrigin: true }));
+app.use('/customers', createProxyMiddleware({ target: 'http://customer-service:3005', changeOrigin: true }));
 
 app.listen(port, () => {
   console.log(`API Gateway running on port ${port}`);
 });
-
